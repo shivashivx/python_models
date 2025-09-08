5cae2fc005350df5b94d2cfb57fcac38ffdd30fb
shiv123 <sivamuzhapilangad@gmail.com>
2025-09-08 09:44:32 +0530

    my fist commit

---
+++ dataTypes.py
@@ -0,0 +1,29 @@
+# # # a=30
+# # # a+=5
+# # # print(a)
+
+# # # a=40
+# # # a%=2
+# # # print(a)
+
+# # a=10
+# # a-=5
+# # print(a)
+
+# # a=7
+# # a*=5
+# # print(a)
+
+# # a=100
+# # a/=2
+# # print(a)
+
+
+# a=5000
+# a%=5
+# print(a)
+
+a,b=10,20
+print(a==b)
+
+print(a>b)
---
+++ inputOutput.py
@@ -0,0 +1,8 @@
+# # name=input("enter your name ")
+# # age=int(input("enter your age"))
+
+# # print(f" your name is {name} and your age is {age}")
+
+# name=input("enter your name ")
+# age=int(input("enter your age"))
+# print("your name is "name",and your "age", ")
